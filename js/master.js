54c41a7b49e8981662af548772c3eceec3aad7ab
AlaaEldinTarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2022-06-20 07:47:42 +0200

    up

--- js/master.js
+++ js/master.js
@@ -1,3 +1,129 @@
+// Check If There's local Storage Color Option
+let mainColor = localStorage.getItem("color_option");
+
+if (mainColor !== null) {
+    document.documentElement.style.setProperty('--maincolor', mainColor);
+
+
+    // Remove Active class From ALL Colors List Item 
+    document.querySelectorAll(".colors-list li").forEach(element => {
+        element.classList.remove("active");
+            
+            // Add Active Class On Element With Data-colors === Local Storage Item
+            if (element.dataset.color === mainColor ) {
+
+                // Add Class Active
+                element.classList.add("active");
+            }
+            
+    });
+};
+
+// Random Background Option
+let backgroundOption = true;
+
+// Variable To Control The Interval
+let bgTheInt;
+
+// Check If There's Local Storage Random Background Item
+let bgLocalItem = localStorage.getItem("bg_option");
+
+if (bgLocalItem !== null) {
+    
+
+    if (bgLocalItem === 'true') {
+        bgLocalItem = true;
+    }else {
+        bgLocalItem = false;
+     }
+        
+    // Remove Active Class From All 
+    document.querySelectorAll(".random-background span").forEach(element => {
+        element.classList.remove("active");
+    });
+    if (bgLocalItem === true) {
+        document.querySelector(".random-background .yes").classList.add("active");
+        newhh();
+    } else {
+        document.querySelector(".random-background .no").classList.add("active");
+    }
+}
+
+// Toggle Spin Class On Icon
+document.querySelector(".spain-seting .sett" ).onclick = function (){
+    
+    // Toggle Class Fa-spin For Rotation On Self
+    this.classList.toggle("fa-spin");
+
+    // Toggle Class Open On Main  Setting Box
+    document.querySelector(".setting-box").classList.toggle("open");
+
+    
+}
+// Close Setting box After Mouse Leaveing
+document.querySelector(".setting-box").onmouseleave = function () {
+    document.querySelector(".setting-box").classList.remove("open");
+    document.querySelector(".sett").classList.remove("fa-spin");
+}
+
+
+// Switch Colors 
+const Colorsli = document.querySelectorAll(".colors-list li");
+
+// Loop On All Li
+Colorsli.forEach(li => {
+
+    // Click On event List Items
+    li.addEventListener("click", (e) => {
+
+        // Set Color On Root
+        document.documentElement.style.setProperty('--maincolor', e.target.dataset.color);
+
+        // Set Color On Local Storage
+        localStorage.setItem("color_option", e.target.dataset.color);
+
+        // Remove Active Class From All Childrens
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active");
+        });
+
+        // Add Active Class On Self 
+        e.target.classList.add("active");
+
+    })
+})
+
+// Switch Background Option 
+const randBackEle = document.querySelectorAll(".random-background span");
+
+// Loop On All spans
+randBackEle.forEach(span => {
+
+    // Click On event span
+    span.addEventListener("click", (e) => {
+
+        // Remove Active Class From All Childrens
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active");
+        });
+
+        // Add Active Class On Self 
+        e.target.classList.add("active");
+
+        if (e.target.dataset.background === 'yes') {
+            backgroundOption = true;
+            newhh();
+            localStorage.setItem("bg_option",true);
+        }else {
+            backgroundOption = false;
+            clearInterval(bgTheInt);
+            localStorage.setItem("bg_option",false);
+        }
+        
+
+    })
+})
+
 // Select Landing Page element 
 let land=document.querySelector(".landing-page");
 
@@ -5,11 +131,19 @@ let land=document.querySelector(".landing-page");
 let imgsArry=["1.jpg","2.jpg","3.jpg","4.jpg"]
 
 
-setInterval(() =>{
-    // Get Random Number
-let randNum = Math.floor(Math.random()*imgsArry.length);
 
-// Change Background Imgs Url 
-land.style.backgroundImage = 'url("/imgs/'+imgsArry[randNum]+'")';
-
-}, 10000);
+// Function Randomize Imgs 
+function newhh() {
+    if (backgroundOption === true) {
+        bgTheInt = setInterval(() => {
+            // Get Random Number
+        let randNum = Math.floor(Math.random() * imgsArry.length);
+        
+        // Change Background Imgs Url 
+        land.style.backgroundImage = 'url("/imgs/' + imgsArry[randNum]+'")';
+        
+        }, 1000);
+    }
+    
+}
+newhh();
